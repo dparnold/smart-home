356844b182c62d6ce13481856a4d4f5c799f12e7
probook <d.p.arnold@gmx.de>
2020-09-29 22:36:24 +0200

    introduced variables for the colors, increased the functionality of the menu bar, added the message option to all the pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,37 @@
 
 const burger = document.querySelector(".burger");
+const cross = document.querySelector(".cross");
 const nav = document.querySelector(".nav-links");
-burger.addEventListener("click", burgerClick);
+const content = document.querySelector(".content");
 
+burger.addEventListener("click", burgerClick);
+cross.addEventListener("click", burgerClick);
+
+// get rid of the closing x when the page is loaded (e.g. when a link is openend)
+cross.classList.toggle('hide');
+// toggles burger and x symbol and opens the menu
 function burgerClick() {
-  nav.classList.toggle('nav-active')
+  nav.classList.toggle('nav-active');
+  cross.classList.toggle('hide');
+  burger.classList.toggle('hide');
+  content.classList.toggle('blur');
 }
 
+
+
+function sendMessage(message, messageTime=1500){
+  if(messageTime==""){
+    messageTime=Math.max(message.length*50,1500);
+  }
+  document.getElementById("message").innerHTML = message;
+  content.classList.toggle("blur");
+  setTimeout(function () { 
+      document.getElementById("message").innerHTML = ""; 
+      content.classList.toggle("blur");
+  }, messageTime);
+}
+
+// not needed anymore
 function loadContent(content) {
   $("#content").load(content + ".html");
   burgerClick();
